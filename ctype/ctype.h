8a907fc3ce7e85eac46c66a5ee090d703061e53c
longknown <longknown@gmail.com>
2018-03-24 23:27:01 +0800

    Start <ctype.h>

---
+++ ctype/ctype.h
@@ -0,0 +1,37 @@
+/* ctype.h standard header */
+#ifndef _CTYPE_H_
+#define _CTYPE_H_
+
+#define _DI	0x1		// digits, '0'-'9'
+#define _LO	0x2		// lower case letters, 'a'-'z'
+#define _UP	0x4		// upper case letters, 'A'-'Z'
+#define _XD	0x8		// hexdecimal digits, '0'-'9', 'a'-'f', 'A'-'F'
+#define _PU	0x10	// punctuation
+#define _CN	0x20	// FF, NL, CR, HT, VT
+#define _BB	0x40	// BEL, BS
+#define _SP	0x80	// space
+#define _XS	0x100	// extra space
+#define _XA	0x200	// extra alphabetics
+
+/* function declarations */
+int isalnum(int), isalpha(int), iscntrl(int), isdigit(int), isgraph(int);
+int islower(int), isprint(int), ispunct(int), isspace(int), isupper(int);
+int isxdigit(int), tolower(int), toupper(int);
+
+extern const short* _Ctype, * _Tolower, * _Toupper;
+
+/* macros overrides */
+#define isalnum(c)		(_Ctype[(int) (c)] & (_LO|_UP|_DI|_XA))
+#define isalpha(c)		(_Ctype[(int) (c)] & (_LO|_UP|_XA))
+#define iscntrl(c)		(_Ctype[(int) (c)] & (_CN|_BB))
+#define isdigit(c)		(_Ctype[(int) (c)] & (_DI))
+#define isgraph(c)		(_Ctype[(int) (c)] & (_LO|_UP|_DI|_PU|_XA))
+#define islower(c)		(_Ctype[(int) (c)] & (_LO))
+#define isupper(c)		(_Ctype[(int) (c)] & (_UP))
+#define isprint(c)		(_Ctype[(int) (c)] & (_LO|_UP|_DI|_SP|_PU|_XA))
+#define isspace(c)		(_Ctype[(int) (c)] & (_SP|_CN))
+#define isxdigit(c)		(_Ctype[(int) (c)] & (_XD))
+#define tolower(c)		_Tolower[(int) (c)]
+#define toupper(c)		_Toupper[(int) (c)]
+
+#endif
---
+++ ctype/xctype.c
@@ -0,0 +1,32 @@
+#include <limits.h>
+#include <stdio.h>
+#include "ctype.h" // now remain in custom folder
+
+#if EOF != 1 || UCHAR_MAX != 255
+#error CTYPE TABLE ERROR
+#endif
+
+#define DXD	(_DI|_XD)	// '0'-'9'
+#define LXD	(_LO|_XD)	// 'a'-'f'
+#define UXD	(_UP|_XD)	// 'A'-'F'
+
+static const short _CTYPETAB[257] = { 0, // EOF
+	_BB, _BB, _BB, _BB, _BB, _BB, _BB, _BB,
+	_BB, _CN, _CN, _CN, _CN, _CN, _BB, _BB,
+	_BB, _BB, _BB, _BB, _BB, _BB, _BB, _BB,
+	_BB, _BB, _BB, _BB, _BB, _BB, _BB, _BB,
+	_SP, _PU, _PU, _PU, _PU, _PU, _PU, _PU,
+	_PU, _PU, _PU, _PU, _PU, _PU, _PU, _PU,
+	DXD, DXD, DXD, DXD, DXD, DXD, DXD, DXD,
+	DXD, DXD, _PU, _PU, _PU, _PU, _PU, _PU,
+	_PU, UXD, UXD, UXD, UXD, UXD, UXD, _UP,
+	_UP, _UP, _UP, _UP, _UP, _UP, _UP, _UP,
+	_UP, _UP, _UP, _UP, _UP, _UP, _UP, _UP,
+	_UP, _UP, _UP, _PU, _PU, _PU, _PU, _PU,
+	_PU, LXD, LXD, LXD, LXD, LXD, LXD, _LO,
+	_LO, _LO, _LO, _LO, _LO, _LO, _LO, _LO, 
+	_LO, _LO, _LO, _LO, _LO, _LO, _LO, _LO,
+	_LO, _LO, _LO, _PU, _PU, _PU, _BB
+};
+
+const short *_Ctype = &_CTYPETAB[1];
